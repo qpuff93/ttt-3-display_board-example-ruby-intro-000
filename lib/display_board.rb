0468ac7a5d204a40f58c97f8a73c0267a78f56cc
Quinnin Leon Evans <quinnin.l.evans@gmail.com>
2019-11-27 23:47:01 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 def display_board
-  puts "display_board"
+
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
-puts "a 3x3 tic tac toe baord"
+
 end
